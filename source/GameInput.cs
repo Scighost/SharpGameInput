6689b711d6d30dfc4343737ecb35f76515ac4545
Nathan <nathanhurst03@gmail.com>
2024-05-19 02:18:03 -0600

    Interfaces and GameInputCreate interop

---
+++ source/GameInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SharpGameInput;
+
+public static partial class GameInput
+{
+    public const ulong CurrentCallbackToken = 0xFFFFFFFFFFFFFFFF;
+    public const ulong InvalidCallbackToken = 0x0000000000000000;
+
+    [LibraryImport("gameinput.dll", EntryPoint = "GameInputCreate")]
+    [UnmanagedCallConv(CallConvs = new[] { typeof(System.Runtime.CompilerServices.CallConvStdcall) })]
+    public static partial int Create(out IGameInput gameInput);
+}

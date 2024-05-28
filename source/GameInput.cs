b3b1e494b7f7e7267ba6c5bf4a44bcb339d3a62f
Nathan <nathanhurst03@gmail.com>
2024-05-27 21:15:26 -0600

    Marshal interfaces manually

--- source/GameInput.cs
+++ source/GameInput.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace SharpGameInput
 {
@@ -10,6 +11,16 @@ namespace SharpGameInput
         public const ulong InvalidCallbackToken = 0x0000000000000000;
 
         [DllImport("gameinput.dll", EntryPoint = "GameInputCreate", CallingConvention = CallingConvention.StdCall)]
-        public static extern int Create(out IGameInput gameInput);
+        private static extern int Create(out IntPtr gameInput);
+
+        public static int Create(out IGameInput gameInput)
+        {
+            int result = Create(out IntPtr handle);
+            if (result >= 0 && handle != IntPtr.Zero)
+                gameInput = new(handle);
+            else
+                gameInput = null!;
+            return result;
+        }
     }
 }
